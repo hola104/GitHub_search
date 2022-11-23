4c369de120899f592da8bcdc6a49f5c7f6752655
hola104 <105869987+hola104@users.noreply.github.com>
2022-11-23 13:43:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+import { Search } from "./modules/search.js";
+import { View } from "./modules/view.js";
+import { Api } from "./modules/api.js";
+
+let api = new Api();
+let app = new Search(new View(), api);
