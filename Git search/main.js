b0406d5bf04eb8c41bd9c2900a611ebf34dd46ee
hola104 <105869987+hola104@users.noreply.github.com>
2022-11-23 13:42:05 +0300

    Delete Git search directory

--- Git search/main.js
+++
@@ -1,6 +0,0 @@
-import { Search } from "./modules/search.js";
-import { View } from "./modules/view.js";
-import { Api } from "./modules/api.js";
-
-let api = new Api();
-let app = new Search(new View(), api);
